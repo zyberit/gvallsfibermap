6c4b70211f459602cab5251760bae4b012174616
Zyberit <hakan@violaberg.nu>
2018-01-05 16:43:54 +0100

    some cleanup

--- gvallsfibermap/views.py
+++ gvallsfibermap/views.py
@@ -14,39 +14,6 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# gcash = {}
-# def fill_gcash():
-#     reader = csv.DictReader(open(BASE_DIR+"/geocodecash.csv"))
-#     for row in reader:
-#         gcash[row['Adress']] = [row['Lat'],row['Lon']]
-# 
-# def get_gcode(adress):
-#     if adress in gcash:
-#         return gcash[adress][0],gcash[adress][1]
-#     else:
-#         return 0,0
-# 
-# mcash = []
-# def fill_mcash():
-#     avdelningar = {"Sagodjuren":"Spårare","Husdjuren":"Spårare","Gosedjuren":"Spårare","Nya spårare att fördela":"Spårare","Fabeldjuren":"Upptäckare","Skogsdjuren":"Upptäckare","Urdjuren":"Äventyrare","Rovdjuren":"Äventyrare","Slow Fox":"Utmanare","Rover":"Rover","Övriga ledare":"Ledare"}
-#     if len(gcash) == 0:
-#         fill_gcash()
-#     reader = csv.DictReader(open(BASE_DIR+"/medlemslista.csv"))
-#     for row in reader:
-#         avd = row['Avdelning']
-#         if avd in avdelningar: 
-#             namn = row['Förnamn']+" "+row['Efternamn']
-#             if row['Födselsdatum'][0:4] < "1992":
-#                 grupp = "Ledare"
-#             else:
-#                 grupp = avdelningar[avd]
-#             adr = row['Adress']+", "+row['Postnr']+" "+row['Postort']+", "+row['Land']
-#             lat,lon = get_gcode(adr)
-#             mcash.append({"namn":namn,"grupp":grupp,"avdelning":avd,"position":(lat,lon)})
-
-# import dropbox
 from openpyxl import load_workbook
 import requests
 from io import BytesIO
